29f457e86d28081cf9081c0ecdbddd19bd00942c
George W F Santos <gweverton21@hotmail.com>
2023-03-08 12:28:58 -0300

    commit state variable name changes

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,28 +1,29 @@
-import { useEffect, useState } from 'react';
-import { VStack } from 'native-base';
+import { useEffect, useState } from "react";
+import { VStack } from "native-base";
 
-import { PRODUCTS } from '../data/products';
+import { PRODUCTS } from "../data/products";
 
-import { Brands } from '../components/Brands';
-import { Products } from '../components/Products';
-import { HomeHeader } from '../components/HomeHeader';
-import { ProductCardProps } from '../components/ProductCard';
+import { Brands } from "../components/Brands";
+import { Products } from "../components/Products";
+import { HomeHeader } from "../components/HomeHeader";
+import { ProductCardProps } from "../components/ProductCard";
 
 export function Home() {
-  const [brandSelected, setBrandSelected] = useState('Nike');
+  const [selectedBrand, setSelectedBrand] = useState("Nike");
   const [products, setProducts] = useState<ProductCardProps[]>([]);
 
   useEffect(() => {
-    const filtered = PRODUCTS.filter(product => product.brand === brandSelected) as ProductCardProps[];
+    const filtered = PRODUCTS.filter(
+      (product) => product.brand === selectedBrand
+    ) as ProductCardProps[];
     setProducts(filtered);
-
-  }, [brandSelected])
+  }, [selectedBrand]);
 
   return (
     <VStack flex={1}>
       <HomeHeader />
-      <Brands onSelect={setBrandSelected} selected={brandSelected} />
-      <Products brand={brandSelected} data={products} />
+      <Brands onSelect={setSelectedBrand} selected={selectedBrand} />
+      <Products brand={selectedBrand} data={products} />
     </VStack>
   );
 }

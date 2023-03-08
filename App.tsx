fa9817fa650a7336d532359f5666b00538b9d9f2
George W F Santos <gweverton21@hotmail.com>
2023-03-08 16:33:39 -0300

    set up OneSignal service for push notifications and implement it for android

--- App.tsx
+++ App.tsx
@@ -1,13 +1,22 @@
-import { StatusBar } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from "react-native";
 
-import { Routes } from './src/routes';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { NativeBaseProvider } from "native-base";
+import OneSignal from "react-native-onesignal";
 
-import { THEME } from './src/theme';
-import { Loading } from './src/components/Loading';
+import { Routes } from "./src/routes";
 
-import { CartContextProvider } from './src/contexts/CartContext';
+import { THEME } from "./src/theme";
+import { Loading } from "./src/components/Loading";
+
+import { CartContextProvider } from "./src/contexts/CartContext";
+import { oneSignalAppId } from "./src/secrets";
+
+OneSignal.setAppId(oneSignalAppId);
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
